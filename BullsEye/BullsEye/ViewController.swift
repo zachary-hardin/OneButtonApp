ee58f9f36f71a7cfb903ad9de0d5399c5bc52161
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2018-01-27 14:30:25 -0600

    Restart game everytime the Hit Me button is touched.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,31 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetValueLabel: UILabel!
     var currentValue: Int = 50
+    var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        currentValue = Int(slider.value)
+
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetValueLabel.text = String(targetValue)
     }
 
     @IBAction func showAlert() {
-        let message: String = "You scored \(currentValue)"
+        let message: String = "The value of the slider is: \(currentValue)"
+            + "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Score:", message: message, preferredStyle: .alert)
-        
-        // UIAlertAction specifies the the action that the user can take. It's the
-        // alert's button title.
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: startNewRound)
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
