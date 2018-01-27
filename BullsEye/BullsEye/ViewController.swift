e7c6b71e02495f60170979866b6dac1a9c89f96c
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2018-01-27 13:52:56 -0600

    Fix initial currentValue value.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currentValue = Int(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
